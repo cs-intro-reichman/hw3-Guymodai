5dc33a8c392592a0d8019205fd7f120ed2b9b1f7
Guy Modai <Guy.modai@post.runi.ac.il>
2025-11-17 22:36:40 +0200

    test 01

--- LoanCalc.java
+++ LoanCalc.java
@@ -28,8 +28,11 @@ public class LoanCalc {
 	// Computes the ending balance of a loan, given the loan amount, the periodical
 	// interest rate (as a percentage), the number of periods (n), and the periodical payment.
 	private static double endBalance(double loan, double rate, int n, double payment) {	
-		// Replace the following statement with your code
-		return 0;
+		for( int i = 0 ; i < n ; i ++)
+		{
+			loan = (loan - payment)*(rate/100 +1);
+		}
+		return loan;
 	}
 	
 	// Uses sequential search to compute an approximation of the periodical payment
@@ -38,8 +41,12 @@ public class LoanCalc {
 	// the number of periods (n), and epsilon, the approximation's accuracy
 	// Side effect: modifies the class variable iterationCounter.
     public static double bruteForceSolver(double loan, double rate, int n, double epsilon) {
-		// Replace the following statement with your code
-		return 0;
+		double increament  = 0;
+		while(Math.abs(endBalance(loan,rate,n,increament)) >= epsilon)
+		{
+			increament +=  0.0001;			
+		}
+		return increament;
     }
     
     // Uses bisection search to compute an approximation of the periodical payment 
@@ -48,7 +55,26 @@ public class LoanCalc {
 	// the number of periods (n), and epsilon, the approximation's accuracy
 	// Side effect: modifies the class variable iterationCounter.
     public static double bisectionSolver(double loan, double rate, int n, double epsilon) {  
-        // Replace the following statement with your code
-		return 0;
-    }
+		double H = loan;
+		double L = 0;
+       // Sets L and H to initial values such that f(L) > 0, f(H) < 0,
+	// implying that the function evaluates to zero somewhere between L and H.
+	// So, let’s assume that L and H were set to such initial values.
+		double g = (L + H)/2;
+	while (H - L > epsilon) {
+		// Sets L and H for the next iteration
+		if (endBalance(loan,rate,n,g) * endBalance(loan,rate,n,L) > 0)
+		{
+			L = g;
+		}
+		// the solution must be between g and H
+		// so set L or H accordingly
+		else
+		{
+			H = g;
+		}
+		g = (L + H)/2;
+		}
+		return (H+L)/2;
+		}
 }
